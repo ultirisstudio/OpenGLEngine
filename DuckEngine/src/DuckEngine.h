79b08d05af2d3863d00866b04431cadd7cbf81fa
Kurogenshi <79649700+Kurogenshi@users.noreply.github.com>
2023-01-24 01:33:16 +0100

    Delete DuckEngine directory

--- DuckEngine/src/DuckEngine.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "DuckEngine/Core/Application.h"
-#include "DuckEngine/Core/Layer.h"
-#include "DuckEngine/ImGui/ImGuiLayer.h"
-#include "DuckEngine/Renderer/Renderer.h"
--- DuckEngine/src/DuckEngine/ImGui/ImGuiBuild.cpp
+++
@@ -1,5 +0,0 @@
-#include "depch.h"
-
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include "examples/imgui_impl_opengl3.h"
-#include "examples/imgui_impl_glfw.cpp"
